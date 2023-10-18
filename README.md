908fbf5929f7565dafec41a02195fc4df952534b
Shuotian Cheng <shuotian@fb.com>
2023-10-18 14:52:43 -0700

    Update the example to use Campaign instead

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # A Sample Project Upon Biz SDK on Java
 
 ```
-docker run -it --rm --name java-dev -v "$(pwd)":/app java-dev
+docker run -d --rm --name java-dev -v "$(pwd)":/app java-dev
 docker exec -it java-dev /bin/bash
 ```
 
 
 # Commands
+
+## Git
+```
+git clone --recursive git@github.com:stcheng/meta-business-sdk-java-sample.git
+git submodule update --recursive
+```
+
+## Maven
 ```
 mvn compile
 mvn exec:java
--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,14 +1,11 @@
 package com.example;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-import com.facebook.ads.sdk.APIContext;
-import com.facebook.ads.sdk.APINodeList;
-
-import com.facebook.ads.sdk.ProductCatalog;
-import com.facebook.ads.sdk.ProductItem;
+import com.facebook.ads.sdk.*;
 
 public class HelloWorld {
     public static final APIContext context = new APIContext(
@@ -19,23 +16,29 @@ public class HelloWorld {
         System.out.println("Hello World");
         try {
 
-            ProductCatalog pc = new ProductCatalog("", context).get().requestFields(Arrays.asList("id", "name")).execute();
-            System.out.println(pc);
-            APINodeList<ProductItem> pis = pc.getProducts().requestFields(Arrays.asList("id","name","capability_to_review_status","retailer_id","retailer_product_group_id")).setParam("limit", 200).execute();
+            AdAccount account = new AdAccount("", context);
+            APINodeList<Campaign> campaigns = account.getCampaigns().execute();
 
-            while (pis != null) {
-                for (ProductItem pi : pis) {
-                    if (pi.getFieldCapabilityToReviewStatus()==null) {
-                        System.out.println(pi);
-                    }
-
-                }
-                pis = pis.nextPage();
+            List<String> ids = new ArrayList<>();
+            for (var campaign : campaigns) {
+                ids.add(campaign.getId());
             }
+
+            APINodeList<Campaign> campaign_details = Campaign.fetchByIds(
+                ids,
+                Arrays.asList(
+                    "id",
+                    "name"
+                ),
+                context
+            );
+
+            for (var campaign : campaign_details) {
+                System.out.println(campaign.getId() + ":" + campaign.getFieldName());
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        System.out.println("----------");
     }
 }
